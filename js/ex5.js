ee5fad56155c885b08a69d5d450fb2d517659c11
Takeo Kimura <takeo.kimura@student.csulb.edu>
2021-02-07 22:21:31 -0800

    Changed the equation of ex5

--- js/ex5.js
+++ js/ex5.js
@@ -1,5 +1,5 @@
 const inputNumber = Number(prompt("Enter a number: "));
 
 for (counter = 0; counter < 11; counter++) {
-  console.log(`${inputNumber}+${counter} = ${inputNumber * counter}`);
+  console.log(`${inputNumber}x${counter} = ${inputNumber * counter}`);
 };
